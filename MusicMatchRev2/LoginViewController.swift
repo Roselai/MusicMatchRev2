86ce1da28243eaf649d5f5f0108fe0543cee5cfc
Shukti Shaikh <tazad320@gmail.com  git config --global user.name Shooktee>
2018-02-07 13:39:47 -0500

    2/7

---
+++ MusicMatchRev2/LoginViewController.swift
@@ -0,0 +1,50 @@
+//
+//  LoginViewController.swift
+//  MusicMatchRev2
+//
+//  Created by Shukti Shaikh on 1/9/18.
+//  Copyright © 2018 Shukti Shaikh. All rights reserved.
+//
+
+
+import UIKit
+
+import SpotifyLogin
+
+class LoginViewController: UIViewController {
+    
+
+    @IBOutlet weak var spotifyLoginButton: UIButton!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    
+        spotifyLoginButton = SpotifyLoginButton(viewController: self, scopes: [.streaming, .userLibraryRead])
+       
+        
+        
+       
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        super.viewWillAppear(animated)
+        
+        SpotifyLogin.shared.getAccessToken { (accessToken, error) in
+            if error != nil {
+                // User is not logged in, show log in flow.
+                print(accessToken)
+                
+            }
+        }
+    }
+    
+    @IBAction func spotifyLoginButtonPressed(_ sender: UIButton) {
+        
+        SpotifyLoginPresenter.login(from: self, scopes: [.streaming, .userLibraryRead])
+    }
+    
+    
+}
